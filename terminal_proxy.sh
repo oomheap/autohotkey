c8db9ec99a9b48f53e98cde861e8f07508fb13b9
cgy <hi34616252@aliyun.com>
2024-01-03 13:33:48 +0800

    perf: update

--- terminal_proxy.sh
+++ terminal_proxy.sh
@@ -2,9 +2,9 @@
 # alias set-proxy='export https_proxy=127.0.0.1:7890 http_proxy=127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890'
 # 开启代理
 function proxy_on(){
-   export ALL_PROXY=socks5://127.0.0.1:7890  # 注意你的端口号可能不是1087，注意修改
-   export http_proxy=http://127.0.0.1:7890
-   export https_proxy=https://127.0.0.1:7890
+   export ALL_PROXY=socks5://127.0.0.1:7789  # 注意你的端口号可能不是1087，注意修改
+   export http_proxy=http://127.0.0.1:7789
+   export https_proxy=https://127.0.0.1:7789
    echo -e "已开启代理"
 }
 
